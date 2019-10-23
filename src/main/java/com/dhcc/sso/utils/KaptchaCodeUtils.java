85dfaf2f477db9ebfd84a9132c6b52d292cbb613
minghao <longyaxx258@163.com>
2019-10-23 18:10:07 +0800

    登录页修改

--- src/main/java/com/dhcc/sso/utils/KaptchaCodeUtils.java
+++ src/main/java/com/dhcc/sso/utils/KaptchaCodeUtils.java
@@ -22,15 +22,16 @@ public class KaptchaCodeUtils {
     public static DefaultKaptcha getDefaultKaptcha() {
         DefaultKaptcha defaultKaptcha = new com.google.code.kaptcha.impl.DefaultKaptcha();
         Properties properties = new Properties();
-        properties.setProperty("kaptcha.border", "yes");
+        properties.setProperty("kaptcha.border", "no");
         properties.setProperty("kaptcha.border.color", "105,179,90");
         properties.setProperty("kaptcha.textproducer.font.color", "blue");
         properties.setProperty("kaptcha.image.width", "110");
-        properties.setProperty("kaptcha.image.height", "40");
+        properties.setProperty("kaptcha.image.height", "35");
         properties.setProperty("kaptcha.textproducer.font.size", "30");
         properties.setProperty("kaptcha.session.key", "code");
         properties.setProperty("kaptcha.textproducer.char.length", "4");
         properties.setProperty("kaptcha.textproducer.font.names", "宋体,楷体,微软雅黑");
+        properties.setProperty("kaptcha.obscurificator.impl", "com.google.code.kaptcha.impl.WaterRipple");
         Config config = new Config(properties);
         defaultKaptcha.setConfig(config);
 
